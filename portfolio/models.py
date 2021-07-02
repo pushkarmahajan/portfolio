4af86877b02b894a9da4fd1c23055733943c2384
Pushkar Mahajan <pushkarmahajan2202.com>
2021-07-02 05:40:18 +0530

    created models

---
+++ portfolio/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):  # inheriting model class
+    # we can use django model fields to see what data type is suitable for us
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    # for using image field we need to install pillow
+    image = models.ImageField(upload_to="portfolio/images")
+    # blank is supported by every field, it is used to specify
+    url = models.URLField(blank=True)
+    # that if a field can be (empty or not) or (mandatory or not)
+
+
+ 
